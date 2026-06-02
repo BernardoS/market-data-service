82dcfd26da73b7076dc73b3733b4ab78518dd436
BernardoS <bernardo.sfs27@gmail.com>
2026-06-02 13:02:37 -0300

    feat: add `MarketDataSyncWorker` structure

---
+++ src/MarketDataService.Workers/MarketDataSyncWorker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Hosting;
+
+namespace MarketDataService.Workers
+{
+    public class MarketDataSyncWorker : BackgroundService
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            TimeSpan interval = TimeSpan.FromSeconds(1);
+
+            using PeriodicTimer timer = new PeriodicTimer(interval);
+
+            while(!stoppingToken.IsCancellationRequested && await timer.WaitForNextTickAsync(stoppingToken))
+            {
+                ExecuteMarketSynchronization();
+            }
+        }
+
+        private void ExecuteMarketSynchronization()
+        {
+            Console.WriteLine($"Sincronizando dados... \n{Guid.NewGuid()}");
+        }
+    }
+}
